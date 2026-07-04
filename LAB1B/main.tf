10280c397b0e652383e1882a3fea7ad04e87b2ea
nyarko-i <inyarko98@gmail.com>
2026-07-04 10:37:53 +0000

    Add Terraform VPC infrastructure with public and private subnets

--- LAB1B/main.tf
+++ LAB1B/main.tf
@@ -1,7 +1,72 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
+#define VPC resource
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Terraform-VPC"
+  }
+}
+
+#Attach internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "VPC-IGW"
+  }
+}
+
+
+#create public and private subnet 
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Private-Subnet"
+  }
+}
+
+# Create Route tables 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
